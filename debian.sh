026776392d16c05f9919dc34cf7b4b5d530ded03
billypom on debian <billypompey@icloud.com>
2024-10-13 11:06:49 -0400

    conditional installing for wayland and gnome

--- debian.sh
+++ debian.sh
@@ -25,38 +25,58 @@ check_dir_exists() {
 }
 
 is_macbook=false
-confirm "Swap Left Super & Left Control? (Mac keyboard)" && is_macbook=true
+install_themes=false
+install_nerdfonts=false
+confirm "\033[94mSwap Left Super & Left Control? (Mac keyboard)\033[0m" && is_macbook=true
+confirm "\033[94mInstall GTK themes?\033[0m" && install_themes=true
+confirm "\0cc[94mInstall Nerd Fonts?\033[0m" && install_nerdfonts=true
 
 sudo apt update
 sudo apt upgrade
-sudo apt install vim git cifs-utils nfs-common ripgrep stow virtualenv wget npm zip unzip kitty libfuse-dev python3-pip pipx nemo breeze-icon-theme gnome-tweaks
 sudo apt purge nano evolution nautilus
-# erdfonts
-bash nerdfonts.sh
-# themes
-# user theme directory
-mkdir -p ~/.themes
-bash colloid-gtk-theme.sh
-bash lavanda-gtk-theme.sh
-# adds ~/.local/bin to PATH
-pipx ensurepath
-# default gnome stuff
-gsettings set org.gnome.desktop.interface gtk-theme Lavanda-Sea-Dark
-gsettings set org.gnome.shell.extensions.user-theme name Lavanda-Sea-Dark
-gsettings set org.gnome.desktop.interface icon-theme breeze
-xdg-mime default nemo.desktop inode/directory
-gsettings set org.gnome.desktop.default-applications.terminal exec ‘kitty’
-if $is_macbook; then 
-    echo "Swapping left Super & left Control"
-    gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:swap_lwin_lctl']"
+sudo apt install vim git cifs-utils nfs-common ripgrep stow virtualenv wget npm zip unzip kitty libfuse-dev python3-pip nemo
+# wayland specific packages
+if "$XDG_SESSION_TYPE" == "wayland"; then
+    echo "Installing wayland specific packages"
+    sudo apt install -y wl-clipboard
 fi
-# install gnome extensions manager, cli
-# https://github.com/essembeh/gnome-extensions-cli
-pipx install gnome-extensions-cli --system-site-packages
-# install gnome extensions
-gext install dash-to-dock@micxgx.gmail.com user-theme@gnome-shell-extensions.gcampax.github.com openbar@neuromorph emoji-copy@felipeftn tiling-assistant@leleat-on-github Vitals@CoreCoding.com compiz-windows-effect@hermes83.github.com
-# enable gnome extensions
-gext enable dash-to-dock@micxgx.gmail.com user-theme@gnome-shell-extensions.gcampax.github.com openbar@neuromorph emoji-copy@felipeftn tiling-assistant@leleat-on-github Vitals@CoreCoding.com compiz-windows-effect@hermes83.github.com
+# gnome specific packages
+if "$DESKTOP_SESSION" == "gnome"; then
+    echo "Installing gnome-specific packages"
+    sudo apt install -y gnome-tweaks
+    gsettings set org.gnome.desktop.interface gtk-theme Lavanda-Sea-Dark
+    gsettings set org.gnome.shell.extensions.user-theme name Lavanda-Sea-Dark
+    gsettings set org.gnome.desktop.interface icon-theme breeze
+    gsettings set org.gnome.desktop.default-applications.terminal exec ‘kitty’
+    if $is_macbook; then 
+        echo "Swapping left Super & left Control"
+        gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:swap_lwin_lctl']"
+    fi
+    sudo apt install -y pipx
+    # adds ~/.local/bin to PATH
+    pipx ensurepath
+    # https://github.com/essembeh/gnome-extensions-cli
+    pipx install gnome-extensions-cli --system-site-packages
+    # install gnome extensions
+    gext install dash-to-dock@micxgx.gmail.com user-theme@gnome-shell-extensions.gcampax.github.com openbar@neuromorph emoji-copy@felipeftn tiling-assistant@leleat-on-github Vitals@CoreCoding.com compiz-windows-effect@hermes83.github.com
+    # enable gnome extensions
+    gext enable dash-to-dock@micxgx.gmail.com user-theme@gnome-shell-extensions.gcampax.github.com openbar@neuromorph emoji-copy@felipeftn tiling-assistant@leleat-on-github Vitals@CoreCoding.com compiz-windows-effect@hermes83.github.com
+fi
+# nerdfonts
+if $install_nerdfonts; then
+    bash nerdfonts.sh
+fi
+# themes
+if $install_themes; then
+    # user theme directory
+    mkdir -p ~/.themes
+    bash colloid-gtk-theme.sh
+    bash lavanda-gtk-theme.sh
+fi
+
+# set default file manager
+xdg-mime default nemo.desktop inode/directory
+
 # install neovim
 mkdir -p ~/applications
 wget -q --show-progress https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
