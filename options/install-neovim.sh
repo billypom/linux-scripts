fb104323bed122d3beef1b124a970a46e07be4ee
billypom on debian <billypompey@icloud.com>
2025-02-01 21:42:23 -0500

    neovim 0.10.4 version pinning and node env for Mason plugins

--- options/install-neovim.sh
+++ options/install-neovim.sh
@@ -1,11 +1,13 @@
+sudo apt install npm
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 mkdir -p ~/applications
-wget -q --show-progress https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-if check_dir_exists ~/applications/nvim-linux64/; then
-    mv ~/applications/nvim-linux64/ ~/applications/nvim-linux64.old/
+wget -q --show-progress https://github.com/neovim/neovim/releases/download/v0.10.4/nvim-linux-x86_64.tar.gz
+if check_dir_exists ~/applications/nvim-linux-x86_64/; then
+    mv ~/applications/nvim-linux-x86_64/ ~/applications/nvim-linux-x86_64.old/
 fi
-tar xzf nvim-linux64.tar.gz
-mv nvim-linux64 ~/applications/
-rm nvim-linux64.tar.gz
-rm -r ~/applications/nvim-linux64.old
-if ! grep -q "alias vim" ~/.bash_aliases; then echo ‘alias vim=“~/applications/nvim-linux64/bin/nvim”’ >> ~/.bash_aliases; fi
+tar xzf nvim-linux-x86_64.tar.gz
+mv nvim-linux-x86_64 ~/applications/
+rm nvim-linux-x86_64.tar.gz
+rm -r ~/applications/nvim-linux-x86_64.old
+if ! grep -q "alias vim" ~/.bash_aliases; then echo "alias vim='~/applications/nvim-linux-x86_64/bin/nvim'" >> ~/.bash_aliases; fi
 echo "Installed latest neovim"
