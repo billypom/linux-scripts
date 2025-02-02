f879490c7b3ee7bf49cd9fe06e9757209e36062c
billypom on debian <billypompey@icloud.com>
2025-02-01 21:11:25 -0500

    server mode

--- debian.sh
+++ debian.sh
@@ -34,6 +34,7 @@ check_dir_exists() {
 }
 
 is_macbook=false
+is_server=true
 install_themes=false
 install_nerdfonts=false
 install_neovim=false
@@ -48,10 +49,15 @@ include_debian_backports=false
 echo "${fg_blue}"
 cat ascii.txt
 echo "${fg_cyan}-----Debian Install Script-----${reset}"
+confirm "Is this a server?" && is_server=true
 confirm "Include debian backports in apt package manager? (Y/N)" && include_debian_backports=true
 confirm "Swap Left Super & Left Control <for mac keyboard> (Y/N)" && is_macbook=true
-if echo $DESKTOP_SESSION | grep -q "gnome"; then
-    confirm "Install gnome extensions, tweaks, and configs? (Y/N)" && install_gnome_configs=true
+if $is_server; then
+    echo ":3"
+else 
+    if echo $DESKTOP_SESSION | grep -q "gnome"; then
+        confirm "Install gnome extensions, tweaks, and configs? (Y/N)" && install_gnome_configs=true
+    fi
 fi
 confirm "Install GTK themes? (Y/N)" && install_themes=true
 confirm "Install Nerd Fonts? (Y/N)" && install_nerdfonts=true
@@ -65,52 +71,61 @@ sudo apt purge nano evolution nautilus
 echo "Installing yummy packages"
 sudo apt install vim git cifs-utils nfs-common ripgrep stow virtualenv wget zip unzip kitty libfuse-dev python3-pip nemo ncdu tldr htop
 
-# wayland specific packages
-if echo $XDG_SESSION_TYPE | grep -q "wayland"; then
-    echo "Installing wayland specific packages"
-    sudo apt install -y wl-clipboard
-fi
-
-# gnome specific packages
-if echo $DESKTOP_SESSION | grep -q "gnome" && $install_gnome_configs; then
-    echo "Installing gnome-specific packages"
-    sudo apt install -y gnome-tweaks
-    gsettings set org.gnome.desktop.interface gtk-theme Lavanda-Sea-Dark
-    gsettings set org.gnome.shell.extensions.user-theme name Lavanda-Sea-Dark
-    gsettings set org.gnome.desktop.interface icon-theme breeze
-    gsettings set org.gnome.desktop.default-applications.terminal exec ‘kitty’
-    # better alt tab functionality
-    echo "Making alt-tab better :)"
-    gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
-    gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab', '<Alt>Above_Tab']"
-    gsettings set org.gnome.desktop.wm.keybindings switch-applications "[]"
-    gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "[]"
-    if $is_macbook; then 
-        echo "Swapping left Super & left Control"
-        bash options/toggle-gnome-macbook-keyboard.sh 1
+if $is_server; then
+    echo ":3"
+else
+    # wayland specific packages
+    if echo $XDG_SESSION_TYPE | grep -q "wayland"; then
+        echo "Installing wayland specific packages"
+        sudo apt install -y wl-clipboard
     fi
-    sudo apt install -y pipx
-    # adds ~/.local/bin to PATH
-    pipx ensurepath
-    # https://github.com/essembeh/gnome-extensions-cli
-    pipx install gnome-extensions-cli --system-site-packages
-    # install gnome extensions
-    gext install dash-to-dock@micxgx.gmail.com user-theme@gnome-shell-extensions.gcampax.github.com openbar@neuromorph emoji-copy@felipeftn tiling-assistant@leleat-on-github Vitals@CoreCoding.com compiz-windows-effect@hermes83.github.com
-    # enable gnome extensions
-    gext enable dash-to-dock@micxgx.gmail.com user-theme@gnome-shell-extensions.gcampax.github.com openbar@neuromorph emoji-copy@felipeftn tiling-assistant@leleat-on-github Vitals@CoreCoding.com compiz-windows-effect@hermes83.github.com
+
+    # gnome specific packages
+    if echo $DESKTOP_SESSION | grep -q "gnome" && $install_gnome_configs; then
+        echo "Installing gnome-specific packages"
+        sudo apt install -y gnome-tweaks
+        gsettings set org.gnome.desktop.interface gtk-theme Lavanda-Sea-Dark
+        gsettings set org.gnome.shell.extensions.user-theme name Lavanda-Sea-Dark
+        gsettings set org.gnome.desktop.interface icon-theme breeze
+        gsettings set org.gnome.desktop.default-applications.terminal exec ‘kitty’
+        # better alt tab functionality
+        echo "Making alt-tab better :)"
+        gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
+        gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab', '<Alt>Above_Tab']"
+        gsettings set org.gnome.desktop.wm.keybindings switch-applications "[]"
+        gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "[]"
+        if $is_macbook; then 
+            echo "Swapping left Super & left Control"
+            bash options/toggle-gnome-macbook-keyboard.sh 1
+        fi
+        sudo apt install -y pipx
+        # adds ~/.local/bin to PATH
+        pipx ensurepath
+        # https://github.com/essembeh/gnome-extensions-cli
+        pipx install gnome-extensions-cli --system-site-packages
+        # install gnome extensions
+        gext install dash-to-dock@micxgx.gmail.com user-theme@gnome-shell-extensions.gcampax.github.com openbar@neuromorph emoji-copy@felipeftn tiling-assistant@leleat-on-github Vitals@CoreCoding.com compiz-windows-effect@hermes83.github.com
+        # enable gnome extensions
+        gext enable dash-to-dock@micxgx.gmail.com user-theme@gnome-shell-extensions.gcampax.github.com openbar@neuromorph emoji-copy@felipeftn tiling-assistant@leleat-on-github Vitals@CoreCoding.com compiz-windows-effect@hermes83.github.com
+    fi
 fi
 # nerdfonts
 if $install_nerdfonts; then
     echo "Installing nerdfonts"
     bash options/install-nerdfonts.sh
 fi
+
 # themes
-if $install_themes; then
-    # user theme directory
-    echo "Installing themes"
-    mkdir -p ~/.themes
-    bash options/install-colloid-gtk-theme.sh
-    bash options/install-lavanda-gtk-theme.sh
+if $is_server; then
+    echo ":3"
+else
+    if $install_themes; then
+        # user theme directory
+        echo "Installing themes"
+        mkdir -p ~/.themes
+        bash options/install-colloid-gtk-theme.sh
+        bash options/install-lavanda-gtk-theme.sh
+    fi
 fi
 
 # set default file manager
